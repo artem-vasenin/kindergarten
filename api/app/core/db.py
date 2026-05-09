a551501953f3a8df9f4e82ad5ecec0985cfd7ee2
rusich <artem.skobar@yandex.ru>
2026-05-09 23:32:49 +0300

    started project

---
+++ api/app/core/db.py
@@ -0,0 +1,36 @@
+from typing import AsyncGenerator, Annotated
+from fastapi import Depends
+
+from app.core.settings import Settings
+from sqlalchemy import select
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncSession,
+    async_sessionmaker,
+)
+
+
+settings = Settings() # type: ignore[call-arg]
+engine = create_async_engine(
+    settings.db_url,
+    echo=False,
+    pool_pre_ping=True,
+)
+session = async_sessionmaker(engine, expire_on_commit=False)
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with session() as s:
+        yield s
+
+async def check(s: AsyncSession):
+    res = await s.execute(select(1))
+    return res.scalar_one()
+
+DbSessionDeps = Annotated[
+    AsyncSession,
+    Depends(get_session),
+]
+
+class Base(DeclarativeBase):
+    ...
---
+++ api/app/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+
+from app.core.db import DbSessionDeps
+# from app.core.middlewares import TimingMW
+# from .project.routes import router as projectRouter
+# from .task.routes import router as taskRouter
+# from .user.routes import router as userRouter
+from app.core.settings import Settings
+
+
+def create_app()->FastAPI:
+    settings = Settings() # type: ignore[call-arg]
+    new_app = FastAPI(
+        title=settings.app.title,
+        description=settings.app.description,
+        version=settings.app.version,
+        openapi_tags=[
+            # {'name': 'Projects', 'description': 'Проекты'},
+            # {'name': 'Tasks', 'description': 'Задачи'},
+            # {'name': 'Auth', 'description': 'Пользователи'},
+        ]
+    )
+    new_app.state.settings = settings
+    # new_app.include_router(projectRouter)
+    # new_app.include_router(taskRouter)
+    # new_app.include_router(userRouter)
+
+    # new_app.add_middleware(TimingMW)
+    return new_app
+
+app = create_app()
+
+@app.get('/')
+async def test(s: DbSessionDeps):
+    from .core.db import check
+    res = await check(s)
+    return {'hello': 'world', 'data': res}
